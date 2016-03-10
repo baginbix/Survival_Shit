7424d82560725930ba3bd15e7edfc7fcc9087750
TheChap <Ystlund@hotmail.com>
2016-03-10 20:39:25 +0100

    Tim har de vackraste ögonen

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class BoardManager : MonoBehaviour {
+
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+        public Count(int min, int max)
+        {
+            this.minimum = min;
+            this.maximum = max;
+        }
+    }
+    public int columns = 20;
+    public int rows = 10; 
+    public int offset;
+    public int offsetValue = 64;
+    public int xPos;
+    public int yPos;
+    public GameObject[] floorTiles;
+    private Transform boardHolder;
+    void InitializeList()
+    {
+
+    }
+
+    void BoardSetup()
+    {
+        offset = 0; 
+        for(int x = 0; x < columns; x++)
+        {
+            for(int y = 0; y < rows; y++)
+            {
+                GameObject toInsantiate = floorTiles[UnityEngine.Random.Range(0, floorTiles.Length)];
+                yPos = y;
+                xPos = x + 1;
+                GameObject instance = Instantiate(toInsantiate, new Vector3(xPos, yPos, 0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+            offset++;
+            if(offset > 1)
+            {
+                offset = 0;
+            }
+        }
+    }
+    public void SetupScence()
+    {
+        BoardSetup();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public BoardManager boardScript;
+	// Use this for initialization
+	void Start ()
+    {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+	void InitGame()
+    {
+        boardScript.SetupScence();
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
